dc1c155ee33918d43cf31c7fb2864df453e551a8
朕与将军解战袍 <1393323447@qq.com>
2022-09-05 20:52:38 +0800

    初步实现 Heap Allocation

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,12 +1,18 @@
 #![no_std]
 #![no_main]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
 
 #[macro_use]
 pub mod logger;
 pub mod interrupts;
 pub mod gdt;
 pub mod memory;
+pub mod allocator;
+
+use core::panic::PanicInfo;
 
 use boot_info::BootInfo;
 
@@ -24,5 +30,24 @@ pub fn init(boot_info: &BootInfo) {
 pub fn hlt_loop() -> ! {
     loop {
         x86_64::instructions::hlt();
+    }
+}
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    unsafe {
+        logger::LOGGER
+            .get()
+            .map(|l| l.force_unlock())
+    };
+    log::error!("{}", info);
+
+    loop {
+        hlt_loop();
     }
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
 pub use kernel::{print, println};
 
-use kernel::{logger, memory};
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 
-use core::panic::PanicInfo;
+use kernel::memory;
 use boot_info::BootInfo;
 use x86_64::{VirtAddr, structures::paging::Page};
 
@@ -30,21 +32,29 @@ pub extern "C" fn _start(boot_info: &'static mut BootInfo) -> ! {
     let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
+    // allocate a number on the heap
+    kernel::allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+    
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+
+
     log::info!("It did not crash!");
 
     kernel::hlt_loop();
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    unsafe {
-        logger::LOGGER
-            .get()
-            .map(|l| l.force_unlock())
-    };
-    log::error!("{}", info);
-
-    loop {
-        kernel::hlt_loop();
-    }
 }
